23a66886213ccde227494b0dacb5a64bec927413
Zycness <Zycness@DESKTOP-0F4UOPG>
2021-11-05 19:15:50 -0300

    Agregar archivos de proyecto.

---
+++ LibroForm/FormLibro.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace LibroForm
+{
+    partial class FormLibro
+    {
+
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.txtPaginas = new System.Windows.Forms.TextBox();
+            this.txtEdicion = new System.Windows.Forms.TextBox();
+            this.txtCiudadyPais = new System.Windows.Forms.TextBox();
+            this.txtEditorial = new System.Windows.Forms.TextBox();
+            this.lblISBN = new System.Windows.Forms.Label();
+            this.lblpaginas = new System.Windows.Forms.Label();
+            this.lbledicion = new System.Windows.Forms.Label();
+            this.lbleditorial = new System.Windows.Forms.Label();
+            this.lblciudadyPais = new System.Windows.Forms.Label();
+            this.lblFechadeEdicion = new System.Windows.Forms.Label();
+            this.botGuardar = new System.Windows.Forms.Button();
+            this.botSalir = new System.Windows.Forms.Button();
+            this.txtFechaDeEdicion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(41, 20);
+            this.lbltitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(53, 20);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Título";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(163, 20);
+            this.txtTitulo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(200, 20);
+            this.txtTitulo.TabIndex = 1;
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAutor.Location = new System.Drawing.Point(41, 55);
+            this.lblAutor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Size = new System.Drawing.Size(53, 20);
+            this.lblAutor.TabIndex = 2;
+            this.lblAutor.Text = "Autor";
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(163, 55);
+            this.txtAutor.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(200, 20);
+            this.txtAutor.TabIndex = 3;
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(163, 85);
+            this.txtISBN.Margin = new System.Windows.Forms.Padding(2);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(200, 20);
+            this.txtISBN.TabIndex = 4;
+            // 
+            // txtPaginas
+            // 
+            this.txtPaginas.Location = new System.Drawing.Point(163, 118);
+            this.txtPaginas.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPaginas.Name = "txtPaginas";
+            this.txtPaginas.Size = new System.Drawing.Size(200, 20);
+            this.txtPaginas.TabIndex = 5;
+            // 
+            // txtEdicion
+            // 
+            this.txtEdicion.Location = new System.Drawing.Point(163, 148);
+            this.txtEdicion.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEdicion.Name = "txtEdicion";
+            this.txtEdicion.Size = new System.Drawing.Size(200, 20);
+            this.txtEdicion.TabIndex = 6;
+            // 
+            // txtCiudadyPais
+            // 
+            this.txtCiudadyPais.Location = new System.Drawing.Point(163, 214);
+            this.txtCiudadyPais.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCiudadyPais.Name = "txtCiudadyPais";
+            this.txtCiudadyPais.Size = new System.Drawing.Size(200, 20);
+            this.txtCiudadyPais.TabIndex = 9;
+            // 
+            // txtEditorial
+            // 
+            this.txtEditorial.Location = new System.Drawing.Point(163, 182);
+            this.txtEditorial.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEditorial.Name = "txtEditorial";
+            this.txtEditorial.Size = new System.Drawing.Size(200, 20);
+            this.txtEditorial.TabIndex = 10;
+            // 
+            // lblISBN
+            // 
+            this.lblISBN.AutoSize = true;
+            this.lblISBN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblISBN.Location = new System.Drawing.Point(41, 85);
+            this.lblISBN.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblISBN.Name = "lblISBN";
+            this.lblISBN.Size = new System.Drawing.Size(51, 20);
+            this.lblISBN.TabIndex = 11;
+            this.lblISBN.Text = "ISBN";
+            // 
+            // lblpaginas
+            // 
+            this.lblpaginas.AutoSize = true;
+            this.lblpaginas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpaginas.Location = new System.Drawing.Point(39, 118);
+            this.lblpaginas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblpaginas.Name = "lblpaginas";
+            this.lblpaginas.Size = new System.Drawing.Size(73, 20);
+            this.lblpaginas.TabIndex = 12;
+            this.lblpaginas.Text = "Páginas";
+            // 
+            // lbledicion
+            // 
+            this.lbledicion.AutoSize = true;
+            this.lbledicion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbledicion.Location = new System.Drawing.Point(39, 148);
+            this.lbledicion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbledicion.Name = "lbledicion";
+            this.lbledicion.Size = new System.Drawing.Size(68, 20);
+            this.lbledicion.TabIndex = 13;
+            this.lbledicion.Text = "Edición";
+            // 
+            // lbleditorial
+            // 
+            this.lbleditorial.AutoSize = true;
+            this.lbleditorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbleditorial.Location = new System.Drawing.Point(34, 182);
+            this.lbleditorial.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbleditorial.Name = "lbleditorial";
+            this.lbleditorial.Size = new System.Drawing.Size(75, 20);
+            this.lbleditorial.TabIndex = 14;
+            this.lbleditorial.Text = "Editorial";
+            // 
+            // lblciudadyPais
+            // 
+            this.lblciudadyPais.AutoSize = true;
+            this.lblciudadyPais.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblciudadyPais.Location = new System.Drawing.Point(11, 214);
+            this.lblciudadyPais.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblciudadyPais.Name = "lblciudadyPais";
+            this.lblciudadyPais.Size = new System.Drawing.Size(117, 20);
+            this.lblciudadyPais.TabIndex = 15;
+            this.lblciudadyPais.Text = "Ciudad y País";
+            // 
+            // lblFechadeEdicion
+            // 
+            this.lblFechadeEdicion.AutoSize = true;
+            this.lblFechadeEdicion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechadeEdicion.Location = new System.Drawing.Point(6, 248);
+            this.lblFechadeEdicion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFechadeEdicion.Name = "lblFechadeEdicion";
+            this.lblFechadeEdicion.Size = new System.Drawing.Size(148, 20);
+            this.lblFechadeEdicion.TabIndex = 17;
+            this.lblFechadeEdicion.Text = "Fecha de Edición";
+            // 
+            // botGuardar
+            // 
+            this.botGuardar.BackColor = System.Drawing.Color.White;
+            this.botGuardar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botGuardar.ForeColor = System.Drawing.Color.Black;
+            this.botGuardar.Location = new System.Drawing.Point(38, 303);
+            this.botGuardar.Name = "botGuardar";
+            this.botGuardar.Size = new System.Drawing.Size(102, 34);
+            this.botGuardar.TabIndex = 12;
+            this.botGuardar.Text = "Guardar";
+            this.botGuardar.UseVisualStyleBackColor = false;
+            this.botGuardar.Click += new System.EventHandler(this.botGuardar_Click);
+            // 
+            // botSalir
+            // 
+            this.botSalir.BackColor = System.Drawing.Color.White;
+            this.botSalir.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botSalir.ForeColor = System.Drawing.Color.Black;
+            this.botSalir.Location = new System.Drawing.Point(233, 303);
+            this.botSalir.Name = "botSalir";
+            this.botSalir.Size = new System.Drawing.Size(111, 34);
+            this.botSalir.TabIndex = 13;
+            this.botSalir.Text = "Salir";
+            this.botSalir.UseVisualStyleBackColor = false;
+            this.botSalir.Click += new System.EventHandler(this.botSalir_Click);
+            // 
+            // txtFechaDeEdicion
+            // 
+            this.txtFechaDeEdicion.Location = new System.Drawing.Point(163, 248);
+            this.txtFechaDeEdicion.Name = "txtFechaDeEdicion";
+            this.txtFechaDeEdicion.Size = new System.Drawing.Size(200, 20);
+            this.txtFechaDeEdicion.TabIndex = 18;
+            // 
+            // FormLibro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(396, 369);
+            this.Controls.Add(this.txtFechaDeEdicion);
+            this.Controls.Add(this.botSalir);
+            this.Controls.Add(this.botGuardar);
+            this.Controls.Add(this.lblFechadeEdicion);
+            this.Controls.Add(this.lblciudadyPais);
+            this.Controls.Add(this.lbleditorial);
+            this.Controls.Add(this.lbledicion);
+            this.Controls.Add(this.lblpaginas);
+            this.Controls.Add(this.lblISBN);
+            this.Controls.Add(this.txtEditorial);
+            this.Controls.Add(this.txtCiudadyPais);
+            this.Controls.Add(this.txtEdicion);
+            this.Controls.Add(this.txtPaginas);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.lblAutor);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.lbltitulo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormLibro";
+            this.Text = "Libro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label lblAutor;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.TextBox txtPaginas;
+        private System.Windows.Forms.TextBox txtEdicion;
+        private System.Windows.Forms.TextBox txtCiudadyPais;
+        private System.Windows.Forms.TextBox txtEditorial;
+        private System.Windows.Forms.Label lblISBN;
+        private System.Windows.Forms.Label lblpaginas;
+        private System.Windows.Forms.Label lbledicion;
+        private System.Windows.Forms.Label lbleditorial;
+        private System.Windows.Forms.Label lblciudadyPais;
+        private System.Windows.Forms.Label lblFechadeEdicion;
+        private System.Windows.Forms.Button botGuardar;
+        private System.Windows.Forms.Button botSalir;
+        private System.Windows.Forms.TextBox txtFechaDeEdicion;
+    }
+}
